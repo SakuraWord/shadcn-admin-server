254b8f61de9e7f550fb76ece25344cfb90beb7d6
liangxiaoxu <1967453332@qq.com>
2026-01-15 22:48:51 +0800

    feat: connect mysql database

--- apps/shadch-admin/src/app.module.ts
+++ apps/shadch-admin/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import {TypeOrmModule} from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule, 
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Lxx@1234',
+      database: 'shadcn_admin',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // 不可在生产中使用，否则可能丢失数据
+      retryDelay: 5000, // 断连时间后重试
+      retryAttempts: 3, // 重联次数
+      autoLoadEntities: true,   // 自动加载实体类
+    }) 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
